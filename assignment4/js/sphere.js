159bd1ed1aafcb0a7bec84f221c471c2af914544
Michael Venturi <michaelventuri58@aol.com>
2021-01-09 20:59:07 -0500

    fixed the volume formula

--- assignment4/js/sphere.js
+++ assignment4/js/sphere.js
@@ -1,6 +1,6 @@
 $("form").on("submit", function (event) {
   event.preventDefault();
   let radius = $("#sphere-radius").val();
-  let volume = (4 / 3) * Math.PI * radius;
+  let volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
   $("#result").html(`${volume} cubic units`);
 });
